e865fe796be024cd1138471b11018d8d3bf2fc7b
djohne <ljashovsky@ukr.net>
2018-03-21 11:08:57 +0200

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,19 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
+from django.http import HttpResponse
+
+
+def demo(request):
+    return HttpResponse("post_list.html"),
+
+def sum(request, a, b):
+    s = int(a) + int(b)
+    return HttpResponse(s)
 
 urlpatterns = [
+    url(r'^[12]+$',demo),
+    url(r'^sum/(?P<a>\d+)/(?P<b>/d+)/$', sum),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
 ]
